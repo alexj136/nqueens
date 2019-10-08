8f57e6c5e4f5fed4a8cfac8d6243edc177a9eeab
Alex Jeffery <apjeffery136@googlemail.com>
2019-10-08 18:10:21 +0100

    started on a class to represent chess boards with queens

--- src/NQueens.java
+++ src/NQueens.java
@@ -2,5 +2,21 @@ public class NQueens {
 
     public static void main (String[] args) {
         System.out.println("Hello Gradle");
+        for(int i = 0; i < 10; i++) {
+            System.out.println(Board.empty(i).nQueens());
+        }
+    }
+}
+
+class Board {
+    private java.util.BitSet bits;
+    public int nQueens() {
+        return (int) Math.sqrt(bits.length());
+    }
+    private Board(java.util.BitSet bits) {
+        this.bits = bits;
+    }
+    public static Board empty(int nQueens) {
+        return new Board(new java.util.BitSet(nQueens * nQueens));
     }
 }
