1d2a575c1e2748123ebf237eafccd63f514522ef
Alex Jeffery <apjeffery136@googlemail.com>
2019-10-10 16:29:00 +0100

    approaching a finished first pass, added maven central repo to gradle file in order to use apache commons Pair class

--- src/NQueens.java
+++ src/NQueens.java
@@ -1,22 +1,133 @@
+import java.util.BitSet;
+import java.util.ArrayList;
+import java.util.Optional;
+import org.apache.commons.lang3.tuple.Pair;
+
 public class NQueens {
 
     public static void main (String[] args) {
-        System.out.println("Hello Gradle");
-        for(int i = 0; i < 10; i++) {
-            System.out.println(Board.empty(i).nQueens());
-        }
+        System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+        System.out.println("-=-=-=-=- YAY IT COMPILES -=-=-=-=-");
+        System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+    }
+
+    public static Optional<Board> boardSearch(int nQueens) {
+        ArrayList<Board> toExplore = new ArrayList<>();
+        ArrayList<Board> solutions = new ArrayList<>();
+        toExplore.add(Board.empty(nQueens));
+        do {
+            Board current = toExplore.remove(0);
+            if(current.placedQueens() >= nQueens) {
+                solutions.add(current);
+            }
+            current.openCell().ifPresent(coord -> {
+                
+            });
+        } while(toExplore.size() > 1);
+        return toExplore.isEmpty() ?
+            Optional.empty() : Optional.of(toExplore.get(0));
     }
 }
 
 class Board {
-    private java.util.BitSet bits;
-    public int nQueens() {
-        return (int) Math.sqrt(bits.length());
-    }
-    private Board(java.util.BitSet bits) {
+
+    private BitSet bits;
+    private ArrayList<Pair<Integer, Integer>> queenCoords;
+    public final int nQueens;
+
+    private Board(int nQueens, BitSet bits) {
+        this.nQueens = nQueens;
         this.bits = bits;
     }
+
     public static Board empty(int nQueens) {
-        return new Board(new java.util.BitSet(nQueens * nQueens));
+        return new Board(nQueens, new BitSet(nQueens * nQueens));
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for(int rw = 0; rw < nQueens; rw++) {
+            for(int cl = 0; cl < nQueens; cl++) {
+                sb.append(queenCoords.contains(Pair.of(rw, cl))?'.':' ');
+            }
+        }
+        return sb.toString();
+    }
+
+    public Board clone() {
+        return new Board(nQueens, (BitSet) bits.clone());
+    }
+
+    public int placedQueens() {
+        return queenCoords.size();
+    }
+
+    private int coordToLinearIndex(Pair<Integer, Integer> coord) {
+        return coord.getLeft() * nQueens + coord.getRight();
+    }
+
+    private Pair<Integer, Integer> linearIndexToCoord(int linearIndex) {
+        return Pair.of(linearIndex / nQueens, linearIndex % nQueens);
+    }
+
+    public boolean occupied(Pair<Integer, Integer> coord) {
+        return bits.get(coordToLinearIndex(coord));
+    }
+
+    public void setOccupied(Pair<Integer, Integer> coord) {
+        bits.set(coordToLinearIndex(coord));
+    }
+
+    public void setOccupied(int row, int column) {
+        bits.set(coordToLinearIndex(Pair.of(row, column)));
+    }
+
+    public void placeQueen(Pair<Integer, Integer> coord) {
+        if(occupied(coord)) {
+            throw new RuntimeException(
+                "Tried to place queen in spot marked as illegal");
+        }
+        else {
+            setPlaceableBits(coord);
+            queenCoords.add(coord);
+        }
+    }
+
+    public Optional<Pair<Integer, Integer>> openCell() {
+        int clearBit = bits.nextClearBit(0);
+        if (clearBit < 0) return Optional.empty();
+        else return Optional.of(linearIndexToCoord(clearBit));
+    }
+
+    public void setPlaceableBits(Pair<Integer, Integer> coord) {
+        setOccupied(coord);
+        int row = coord.getLeft(), column = coord.getRight();
+
+        // Vertical
+        for(int rw = 0; rw < row; rw++) setOccupied(rw, column);
+        for(int rw = row + 1; rw < nQueens; rw++) setOccupied(rw, column);
+
+        // Horizontal
+        for(int cl = 0; cl < column; cl++) setOccupied(row, cl);
+        for(int cl = column + 1; cl < nQueens; cl++) setOccupied(row, cl);
+
+        // Diagonal UL -> BR
+        for(int rw = row - 1, cl = column - 1; rw >= 0 && cl >= 0; rw--, cl--)
+            setOccupied(rw, cl);
+        for(int rw = row + 1, cl = column + 1;
+            rw < nQueens && cl < nQueens; rw++, cl++) setOccupied(rw, cl);
+
+        // Diagonal UR -> BL
+        for(int rw = row - 1, cl = column + 1;
+            rw >= 0 && cl < nQueens; rw--, cl++) setOccupied(rw, cl);
+        for(int rw = row + 1, cl = column - 1;
+                rw < nQueens && cl >= 0; rw++, cl--) setOccupied(rw, cl);
+
+        /*for(Pair<Integer, Integer> coord : queenCoords) {
+            int rw = coord.getLeft(), cl = coord.getRight();
+        }
+
+        throw new RuntimeException("Not yet implemented");*/
     }
 }
